25ff1c91e167fb99b209e8212b01dc54700ae6c2
Mario <mariommtech@gmail.com>
2022-03-26 14:53:44 +0100

    Modificado: BestFitter.sh y MainGraph.sh

--- GraphKiller/BestFitter.sh
+++ GraphKiller/BestFitter.sh
@@ -3,8 +3,9 @@
 # Archive: BestFitter.sh
 # Input:
 # 1. Archive with the information of all the regresions obtain in 
-#    Regresions.ss, including the fit function, the parameters of the 
-#    function and the ERM.
+#    Regresions.sh, including the fit function, the parameters of the 
+#    function and the ERM. <name1.txt>
+# 2. The name of the output file <name2.txt>
 # Output:
 # 1. Archieve that contais the information of the best type of adjust.
 #######################################################################
@@ -14,14 +15,14 @@
 
 # Now, we have to determinate which is the maximum value
 
-maxERM=-1
+maxERM=1
 
 cut -d ' ' -f 3 $1 | cut -d ":" -f 2 | while IFS= read -r line 
 do
 	echo "Linea leída: $line"
-	if [ $line > $maxERM ];
+	if [[ $line > $maxERM ]];
 	then
-		maxERM=$line;
+		maxERM=$line
 	fi;
 done
 
