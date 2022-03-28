475fcaff3058064af8b2eff38b755e63b106daeb
Samerks <92316452+Samerks@users.noreply.github.com>
2022-03-29 00:17:28 +0200

    Update GraphPlotter.sh

--- GraphKiller/GraphPlotter.sh
+++ GraphKiller/GraphPlotter.sh
@@ -2,11 +2,9 @@
 
 # ARGUMENTS table : regresion : título : eje x (label:min:max) : eje y label : salida 
 
-best_fitter=`./BestFitter.sh $2 $2.dat`
-
 table=$1
-regresion=`cat "$2.dat" | cut -d ":" -f 3 | awk '{ print substr($0, 6, length($0) - 8) }' `
-title=`cat "$2.dat" | cut -d ":" -f 2`
+regresion=`cat "$2" | cut -d ":" -f 3 | awk '{ print substr($0, 6, length($0) - 8) }' `
+title=$3
 label_x=$4
 min_x=$5
 max_x=$6
@@ -23,5 +21,3 @@ gnuplot -persist <<-EOFMarker
     plot "$table" with linespoints , $regresion with linespoints
 
 EOFMarker
-
-rm $2.dat
