b5fe6a03346fcfd79894aabfd9f5ee1eb4fbc98e
Jesus Samuel <e.samugc@go.ugr.es>
2022-04-27 16:06:06 +0200

    s

--- Practica_2/GraphP2.sh
+++ Practica_2/GraphP2.sh
@@ -6,26 +6,55 @@
 # Return: Nothing
 ######################################################################
 
+REPETICIONES=30
+
 # Copy the scripts from ../GraphKiller, so we have them in the current directory of GraphP2.sh
 cp ../GraphKiller/*.sh .
 
-g++ -O3 src/ejercicio-1-comp-fija-no-repetidos.cpp -o ejercicio-1-comp-fija-no-repetidos
-./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 1 --max-x 1001 --num-points 20  --repetitions-per-point 1 ejercicio-1-comp-fija-no-repetidos
-rm ejercicio-1-comp-fija-no-repetidos
+# GRUPO 1
+g++ src/ejercicio-1-comp-fija-repetidos.cpp -o ejercicio-1-comp-fija-repetidos-comparado
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 21 --num-points 10  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-repetidos-comparado
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 25 --max-x 1000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-repetidos-comparado
+rm ejercicio-1-comp-fija-repetidos-comparado
 
-g++ -O3 src/ejercicio-1-comp-fija-repetidos.cpp -o ejercicio-1-comp-fija-repetidos
-./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 1001 --num-points 20  --repetitions-per-point 20 ejercicio-1-comp-fija-repetidos
+# GRUPO 2
+g++ src/ejercicio-1-comp-fija-repetidos.cpp -o ejercicio-1-comp-fija-repetidos
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 2 --num-points 1  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-repetidos
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 250000 --max-x 10000000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-repetidos
 rm ejercicio-1-comp-fija-repetidos
 
-g++ -O3 src/ejercicio-2-mezcla.cpp -o ejercicio-2-mezcla
-./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 1001 --num-points 20  --repetitions-per-point 20 ejercicio-2-mezcla
-rm ejercicio-2-mezcla
+# GRUPO 3
+g++ src/ejercicio-1-comp-fija-repetidos.cpp -D UMBRAL=1000000000 -o ejercicio-1-comp-fija-repetidos-lineal
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 2 --num-points 1  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-repetidos-lineal
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 250000 --max-x 10000000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-repetidos-lineal
+rm ejercicio-1-comp-fija-repetidos
 
-#g++ -O3 src/seleccion-peor.cpp -o seleccion-peor
-#./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x #1 --max-x 45050 --num-points 25  --repetitions-per-point 1 seleccion-peor
-#rm seleccion-peor
+# GRUPO 4
+g++ src/ejercicio-2-mezcla.cpp -o ejercicio-2-mezcla-n
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 2 --num-points 1  --repetitions-per-point $REPETICIONES ejercicio-2-mezcla-n
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en milisegundos)" --min-x 250000 --max-x 10000000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-2-mezcla-n
+rm ejercicio-2-mezcla-n
 
-# Delete the scripts imported from ../GraphKiller.
-rm {BestFitter,GraphPlotter,PointsPlotter,MainGraph,Regressions,TableGenerator}.sh
+# GRUPO 5
+g++ -D KELEMENTS src/ejercicio-2-mezcla.cpp -o ejercicio-2-mezcla-k
+./MainGraph.sh --xlabel "Número de vectores" --ylabel "Tiempo (en milisegundos)" --min-x 1 --max-x 2 --num-points 1  --repetitions-per-point $REPETICIONES ejercicio-2-mezcla-k
+./MainGraph.sh --xlabel "Número de vectores" --ylabel "Tiempo (en milisegundos)" --min-x 250 --max-x 10000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-2-mezcla-k
+rm ejercicio-2-mezcla-k
 
-### END
+# GRUPO 6
+g++ -O1 src/ejercicio-1-comp-fija-no-repetidos-hacked.cpp -o ejercicio-1-comp-fija-no-repetidos-comparado-hacked
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 1 --max-x 21 --num-points 10  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-no-repetidos-comparado-hacked
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 25 --max-x 1000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-no-repetidos-comparado-hacked
+rm ejercicio-1-comp-fija-no-repetidos-comparado-hacked
+
+# GRUPO 7
+g++ -O1 src/ejercicio-1-comp-fija-no-repetidos-hacked.cpp -D UMBRAL=50000000 -o ejercicio-1-comp-fija-busqueda-lineal
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 1 --max-x 21 --num-points 10  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-busqueda-lineal
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 250000 --max-x 10000000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-busqueda-lineal
+rm ejercicio-1-comp-fija-busqueda-lineal
+
+# GRUPO 8
+g++ -D REPETITIONS=50 -D SIZE_VECTOR=2000 src/ejercicio-1-comp-fija-no-repetidos.cpp -o ejercicio-1-comp-fija-no-repetidos
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 1 --max-x 2 --num-points 1  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-no-repetidos
+./MainGraph.sh --xlabel "Número de componentes del vector" --ylabel "Tiempo (en nanosegundos)" --min-x 25 --max-x 1000 --num-points 39  --repetitions-per-point $REPETICIONES ejercicio-1-comp-fija-no-repetidos
+rm ejercicio
