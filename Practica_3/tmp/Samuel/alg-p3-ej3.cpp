35c2f3befbe2b95c1831144348e417448d27645c
Samerks <92316452+Samerks@users.noreply.github.com>
2022-05-17 19:12:41 +0200

    Add files via upload

--- Practica_3/tmp/Samuel/alg-p3-ej3.cpp
+++ Practica_3/tmp/Samuel/alg-p3-ej3.cpp
@@ -2,16 +2,81 @@
 #include <fstream>
 #include<vector>
 #include<cmath>
+#include<algorithm>
 
 using namespace std;
 
+struct Point{
+    double x;
+    double y;
+};
+
+int nearest_point(vector<int> v,vector<int> candidates){
+
+    int pos_min;
+    int min = 300000000;
+
+    for (int i=0; i < v.size();++i){ 
+        if(v.at(i) != 0 && min>v.at(i) && (find(candidates.begin(),candidates.end(),i)==candidates.end())){
+            min=v.at(i);
+            pos_min = i;
+        }
+    }
+    return pos_min;
+}
+    
+vector<int> tsp_near_neighbor(vector<vector<int>> v){
+
+    vector<int> candidates,result;
+    int size = v.at(0).size();
+    int index;
+    int i=0;
+    
+    candidates.push_back(i);
+    while(candidates.size() < size){
+        index = nearest_point(v.at(i),candidates);
+        candidates.push_back(index);
+        i=index;
+    }
+
+    return candidates;
+}
+
+vector<vector<int>> distance(vector<Point> p){
+
+    vector<Point>::iterator it,it1;
+    vector<vector<int>> matrix;
+    vector<Point> copy = p;
+
+
+    for (it=p.begin();it!=p.end();++it){
+        vector<int> vector;
+        for (it1=copy.begin();it1!=copy.end();++it1){   
+            double d = sqrt(pow((*it1).x-(*it).x,2) + pow((*it1).y-(*it).y,2));
+            int dist = trunc(d);
+            vector.push_back(dist);
+        }
+        matrix.push_back(vector);
+    }
+    return matrix;
+}
+
+void toString(vector<vector<int>> matrix){
+    for (auto it=matrix.begin();it!=matrix.end();++it){
+        for (int i=0;i<matrix.size();++i){   
+            cout<<(*it).at(i)<<"  ";
+        }
+        cout<<endl;
+    }
+}
+
+void toString(vector<int> v){
+    for (auto it=v.begin();it!=v.end();++it){
+        cout<<(*it)<<"  ";
+    }
+}
+
 int main(int argc,char *argv[]){
-
-
-    struct Point{
-        double x;
-        double y;
-    };
 
     char cadena[128];
     int dimension;
@@ -22,7 +87,7 @@ int main(int argc,char *argv[]){
     fe >> cadena;
     dimension=stoi(cadena);
 
-    cout<<dimension<<endl;
+    cout<<"Dimensión: "<<dimension<<endl;
 
     int i = 0;
     int pos = 0;
@@ -47,30 +112,17 @@ int main(int argc,char *argv[]){
 
     fe.close();
 
-    copy = p;
+    vector<vector<int>> matrix=distance(p);
 
-    vector<vector<int>> matrix;
+    cout<<"Matriz: "<<endl;
+    toString(matrix);
 
-    vector<Point>::iterator it,it1;
-
-    for (it=p.begin();it!=p.end();++it){
-        vector<int> vector;
-        for (it1=copy.begin();it1!=copy.end();++it1){   
-            double d = sqrt(pow((*it1).x-(*it).x,2) + pow((*it1).y-(*it).y,2));
-            int dist = trunc(d);
-            vector.push_back(dist);
-        }
-        matrix.push_back(vector);
-    }
-
-    for (auto it=matrix.begin();it!=matrix.end();++it){
-        for (int i=0;i<matrix.size();++i){   
-            cout<<(*it).at(i)<<"  ";
-        }
-        cout<<endl;
-    }
-
-
+    vector<int> v;
+    v=tsp_near_neighbor(matrix);
+    cout<<endl<<endl;
+    cout<<"Vector resultante para TSP: "<<endl;
+    toString(v);
+    cout<<endl<<endl;
 
     return 0;
 }
